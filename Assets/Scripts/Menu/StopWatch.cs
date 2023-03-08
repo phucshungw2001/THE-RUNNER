3e713539389a8c7b8e55daf28373caf2d7830a54
Handuong <handuong707@gmail.com>
2023-03-08 23:08:25 +0700

    PauseMenu&Timer

---
+++ Assets/Scripts/Menu/StopWatch.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopWatch : MonoBehaviour
+{
+    public float timeStart;
+    public Text textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString("F2");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart += Time.deltaTime;
+        textBox.text = timeStart.ToString("F2");
+    }
+}
