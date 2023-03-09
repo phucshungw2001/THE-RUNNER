5219b6816e097ba371ee521c01f460edaf328e12
RoyTun <phucshungw2001@gmail.com>
2023-03-09 17:25:43 +0700

    update move player John

---
+++ Assets/Scripts/Character/Mia/PlayerMiaMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMiaMove : MonoBehaviour
+{
+    [SerializeField]
+    private FloatingJoystick joyStick;
+    [SerializeField]
+    private SpriteRenderer sprite;
+    private Animator playerAnimator;
+
+    Vector2 move;
+    Rigidbody2D rb2D;
+
+    public float moveSpeed = 2f;
+
+    PlayerMove player = new PlayerMove();
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move.x = joyStick.Horizontal;
+        move.y = joyStick.Vertical;
+    }
+
+    private void FixedUpdate()
+    {
+        // Call funtion PlayerMoveCommon in class PlayerMove
+        player.PlayerMoveCommon(playerAnimator, "Run", rb2D, move, sprite, moveSpeed);
+    }
+}
