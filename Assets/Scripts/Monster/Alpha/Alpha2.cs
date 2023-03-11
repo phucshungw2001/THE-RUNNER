2d304f1f57c273b1d9e3a1f840d05f9971a839ee
Cuong Quang Bui <Cuongbui0110@gmail.com>
2023-03-11 16:59:57 +0700

    createmonsterA2

---
+++ Assets/Scripts/Monster/Alpha/Alpha2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alpha2 : MonoBehaviour
+{
+    private float damage2 = 1;
+    private float blood2 = 70;
+    private float speed2 = 1;
+    private float attackSpeed2 = 1;
+    private float exp2 = 5;
+
+    public float Damage2 { get => damage2; set => damage2 = value; }
+    public float Blood2 { get => blood2; set => blood2 = value; }
+    public float Speed2 { get => speed2; set => speed2 = value; }
+    public float AttackSpeed2 { get => attackSpeed2; set => attackSpeed2 = value; }
+    public float Exp2 { get => exp2; set => exp2 = value; }
+}
