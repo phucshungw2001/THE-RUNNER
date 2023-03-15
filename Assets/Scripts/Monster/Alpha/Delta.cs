691eab5423c8f894989e7f844074426ba498d086
Cuong Quang Bui <Cuongbui0110@gmail.com>
2023-03-16 02:49:10 +0700

    create boss Delta

---
+++ Assets/Scripts/Monster/Alpha/Delta.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delta : MonoBehaviour
+{
+    private float damage3 = 15;
+    private float blood3 = 600;
+    private float speed3 = 1;
+    private float attackSpeed3 = 1;
+    private float exp3 = 4;
+    private float AttackRangea3 = 1;
+
+    public float Damage3 { get => damage3; set => damage3 = value; }
+    public float Blood3 { get => blood3; set => blood3 = value; }
+    public float Speed3 { get => speed3; set => speed3 = value; }
+    public float AttackSpeed3 { get => attackSpeed3; set => attackSpeed3 = value; }
+    public float Exp3 { get => exp3; set => exp3 = value; }
+    public float AttackRangea31 { get => AttackRangea3; set => AttackRangea3 = value; }
+}
